3f17df77f634f77fe55891e5341d03f5c7c05325
Brey-Quinchia <brehinerqr27@gmail.com>
2022-09-25 01:08:34 -0500

    git push

--- show_data.php
+++ show_data.php
@@ -9,10 +9,19 @@
 </head>
 <body>
     <?php
+    #Necesitamos conectar con nuestra base de datos 
+    include ('includes/conectiondb.php'); 
     copy($_FILES['upload_excel']['tmp_name'],$_FILES['upload_excel']['name']);
     echo ("El archivo se registro en el servidor.<br>"); 
     $nom = $_FILES['upload_excel']['name'];
-    echo "<a href=\"$nom\">This is a File</a>"
+    #Debemo crea una instancia mysql que permita cargar el contenido del archivo dentro de la base de datos
+    /*En  definitiva para cargar los archivos, visualizarlo y exportarlos lo hare mediante las liberias de php dispuestas para este tipo de funciones como:
+        -. PHPExcel ->(https://youtu.be/4Sw24E5Hi5M)
+        -. PHPSpreadSheets -> (https://youtu.be/LAHkGTwnA9g)
+            📑 PhpSpreadsheet: https://phpspreadsheet.readthedocs.io
+            💻 Instalar PhpSpreadsheet: https://youtu.be/hWChLMQ5iFc 
+            💻 Tutorial para composer: https://youtu.be/NGvfsCOVzwo
+    */
     ?>
 </body>
 </html>
