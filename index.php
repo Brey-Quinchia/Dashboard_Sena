1fa48a0548f09e18d3055ca19ea6d36452b94a99
Brey-Quinchia <brehinerqr27@gmail.com>
2022-09-27 17:34:02 -0500

    git commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require ('includes/styles.html')?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>|Options</title>
+</head>
+<body>
+    <?php require ('includes/menu.html')?>
+    <section class="container">
+        <div class="main">
+            
+        </div>
+    </section>
+</body>
+</html>
