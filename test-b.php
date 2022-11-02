48af1b8b8eee76ee14372c9d44d9b9a420b0e92d
Brey-Quinchia <brehinerqr27@gmail.com>
2022-11-01 20:23:19 -0500

    git commit

--- test-b.php
+++ test-b.php
@@ -1,25 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Conversion de fecha</title>
-</head>
-<body>
-    <h2>Acortar un string</h2>
-    <p>Los valores son arrojados asi <b>2022-08-20 00:00:00.0</b></p>
+
     <?php
     //? Valores de fecha arrojados por los formatos
-    $origindate = "2022-08-20 00:00:00.0"; 
-    echo $origindate . "<br>"; 
-    //? Para conocer la longitud de la fecha entregada
-    echo strlen($origindate); 
-    //? Necesito transformar la fecha utilizando substr
-    $nedDate = substr($origindate, 0,11); 
-    //? problema resuelto
-    echo $nedDate
-
+    #Necesito crear una funcion que devuelva el valor correcto de la fecha 
+    function transform_date($originDate){
+        $correctDate = substr($originDate,0,19); 
+        return $correctDate; 
+    }
     ?>
-</body>
-</html>
